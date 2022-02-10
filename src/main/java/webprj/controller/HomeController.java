136831068151e82185f7647c5ae4aa41f30f39de
sanghyeon <royseo98@gmail.com>
2022-02-11 00:36:09 +0900

    22.02.11 add controller annotation

---
+++ src/main/java/webprj/controller/HomeController.java
@@ -0,0 +1,26 @@
+package webprj.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@RequestMapping("index")
+	public String index() {
+		System.out.println("fsfs");
+		return "root.index";
+	}
+
+//	@Override
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		// TODO Auto-generated method stub
+//		ModelAndView mv = new ModelAndView("root.index");
+//		mv.addObject("data", "Hello Spring MVC~");
+//		//mv.setViewName("/WEB-INF/view/index.jsp");
+//		return mv;
+//	}
+}
+
+//56°­
